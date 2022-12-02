2bf4288b9b75fdda7b5c5e72cf3fe24383cad25c
dilekrc <119048289+dilekrc@users.noreply.github.com>
2022-12-02 13:01:46 +0100

    Update
    Anfang von Feld erstellt, Spieler müssen noch gemacht werden

--- datenstrukturen.c
+++ datenstrukturen.c
@@ -1,1 +1,16 @@
-//test 123
+/*
+
+Name        : datenstrunkturen.c
+
+Authoren    : Dilek Karaca, Kevin Fach
+
+Beschreibung : Erstellung vom Spielfeld und initalisieurng von Spielern.
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Erstellt ein zweidimensionalen Feld mit 9 Bereichen
+int feld[3][3]; 
